3cf79af2b2569388ea333ae054634ae388db2268
wujiaming <289451937@qq.com>
2017-06-22 14:31:41 +0800

    init

--- src/main/java/com/wsj/repository/HelloRepository.java
+++ src/main/java/com/wsj/repository/HelloRepository.java
@@ -8,6 +8,10 @@ import org.springframework.data.jpa.repository.Query;
  * Created by Jimmy on 2017/6/22.
  */
 public interface HelloRepository extends JpaRepository<Companys, Integer> {
+
+
     @Query(value = "select name from Companys where id =?1")
     String findCompanyName(int id);
+
+
 }
