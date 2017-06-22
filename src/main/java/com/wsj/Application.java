b0f82f36e18665d81038da78c1c0055aa1c7e9be
wujiaming <289451937@qq.com>
2017-06-22 16:14:25 +0800

    init

--- src/main/java/com/wsj/Application.java
+++ src/main/java/com/wsj/Application.java
@@ -2,12 +2,19 @@ package com.wsj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by Jimmy on 2017/6/22.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
