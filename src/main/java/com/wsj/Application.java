76c92b05e1be93116c0dba06eb99e2a26e58b50d
wujiaming <289451937@qq.com>
2017-06-22 10:26:44 +0800

    init

---
+++ src/main/java/com/wsj/Application.java
@@ -0,0 +1,14 @@
+package com.wsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Jimmy on 2017/6/22.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
