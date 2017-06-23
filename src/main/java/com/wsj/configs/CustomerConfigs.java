e5619711320d1927b4d4971c1b64ff20f5d21fb2
wujiaming <289451937@qq.com>
2017-06-23 19:36:27 +0800

    init

--- src/main/java/com/wsj/configs/CustomerConfigs.java
+++ src/main/java/com/wsj/configs/CustomerConfigs.java
@@ -1,8 +1,5 @@
 package com.wsj.configs;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.http.converter.HttpMessageConverter;
