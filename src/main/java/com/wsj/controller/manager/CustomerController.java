572b1c0d1493a97aee61e2da16b6d36b6a77666c
wujiaming <wujiaming@iplas.com.cn>
2017-06-25 14:01:27 +0800

    实体类定义

---
+++ src/main/java/com/wsj/controller/manager/CustomerController.java
@@ -0,0 +1,12 @@
+package com.wsj.controller.manager;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jimmy on 2017/6/25.
+ */
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+}
