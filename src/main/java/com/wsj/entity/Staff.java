b392875554fce595dc500cd02722fc2ba2259ed4
wujiaming <wujiaming@iplas.com.cn>
2017-06-25 12:55:29 +0800

    实体类定义

--- src/main/java/com/wsj/entity/Staff.java
+++ src/main/java/com/wsj/entity/Staff.java
@@ -17,12 +17,20 @@ public class Staff {
     private String name;
     @Column(name = "login_name")
     private String loginName;
+    @Column(name="password")
+    private String password;
     @Column(name = "created_at")
     private Date createdAt;
+    @Column(name="created_by")
+    private Integer createdBy;
     @Column(name = "updated_at")
     private Date updatedAt;
-    @Column(name = "last_login_time")
-    private Date lastLoginTime;
+    @Column(name="updated_by")
+    private Integer updatedBy;
+    @Column(name = "last_login_at")
+    private Date lastLoginAt;
+    @Column(name="status")
+    private Integer status;
 
     public Integer getId() {
         return id;
@@ -31,7 +39,6 @@ public class Staff {
     public void setId(Integer id) {
         this.id = id;
     }
-
 
     public String getName() {
         return name;
@@ -49,12 +56,28 @@ public class Staff {
         this.loginName = loginName;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public Date getCreatedAt() {
         return createdAt;
     }
 
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
     }
 
     public Date getUpdatedAt() {
@@ -65,11 +88,27 @@ public class Staff {
         this.updatedAt = updatedAt;
     }
 
-    public Date getLastLoginTime() {
-        return lastLoginTime;
+    public Integer getUpdatedBy() {
+        return updatedBy;
     }
 
-    public void setLastLoginTime(Date lastLoginTime) {
-        this.lastLoginTime = lastLoginTime;
+    public void setUpdatedBy(Integer updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Date getLastLoginAt() {
+        return lastLoginAt;
+    }
+
+    public void setLastLoginAt(Date lastLoginAt) {
+        this.lastLoginAt = lastLoginAt;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
     }
 }
