c153c0b85e37d9b2d7665cf144e192a5de6cd470
wujiaming <289451937@qq.com>
2017-06-27 14:35:49 +0800

    重新排版

--- src/main/java/com/wsj/tools/WsjTools.java
+++ src/main/java/com/wsj/tools/WsjTools.java
@@ -6,6 +6,11 @@ import javax.servlet.http.HttpServletRequest;
  * Created by Jimmy on 2017/6/27.
  */
 public class WsjTools {
+    /**
+     * 获取域名
+     * @param request
+     * @return
+     */
     public static String getDomainName(HttpServletRequest request) {
         StringBuffer url = request.getRequestURL();
         return url.delete(url.length() - request.getRequestURI().length(), url.length()).append("/").toString();
