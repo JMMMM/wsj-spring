db4efc26cb4cb422e8570cf4d13015ab4ba3a3d7
wujiaming <wujiaming@iplas.com.cn>
2017-07-07 21:23:54 +0800

    生成短信接口

--- src/main/java/com/wsj/sms/services/impl/SmsLogServiceImpl.java
+++ src/main/java/com/wsj/sms/services/impl/SmsLogServiceImpl.java
@@ -6,6 +6,8 @@ import com.wsj.sms.services.SmsLogService;
 import com.wsj.sys.bean.ResultBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.xml.transform.Result;
+
 /**
  * Created by jimmy on 2017/7/7.
  */
@@ -15,8 +17,9 @@ public class SmsLogServiceImpl implements SmsLogService {
 
     @Override
     public ResultBean<SmsLog> saveSmsLog(SmsLog smsLog) {
-        if (smsLog.getId() > 0) return null;
+        if (smsLog.getId() > 0) return ResultBean.failure("已存在短信");
+        if (smsLog.getType() > 0) return ResultBean.failure("短信类型不能为空");
         SmsLog log = smsLogRepository.save(smsLog);
-        return ResultBean.success("保存成功",log);
+        return ResultBean.success("保存成功", log);
     }
 }
