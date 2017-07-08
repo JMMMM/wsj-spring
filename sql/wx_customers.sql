b92b01780a9378cebbfcdc268ed6e8f727d8db11
wujiaming <wujiaming@iplas.com.cn>
2017-07-08 21:41:09 +0800

    生成短信接口

--- sql/wx_customers.sql
+++ sql/wx_customers.sql
@@ -2,7 +2,7 @@ drop table if exists new_wsj.wx_customers;
 create table new_wsj.wx_customers(
  `id` int(8) not null comment '用户ID' primary key AUTO_INCREMENT,
  `subscribe` int(8) default null comment '用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。',
- `openid` varchar(255) not null comment '用户的标识，对当前公众号唯一',
+ `openid` varchar(255) not null comment '用户的标识，对当前公众号唯一' UNIQUE ,
  `nickname` varchar(255) not null comment '微信用户昵称' UNIQUE ,
  `nickname_emoji` varchar(255) default null comment '昵称 表情转义',
  `sex` SMALLINT (1) default null comment '用户的性别，值为1时是男性，值为2时是女性，值为0时是未知',
