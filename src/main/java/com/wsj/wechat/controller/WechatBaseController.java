cf796b7da923a8780d0e7a8c989ff147bfe7c317
wujiaming <wujiaming@iplas.com.cn>
2017-07-09 13:33:04 +0800

    生成短信接口

--- src/main/java/com/wsj/wechat/controller/WechatBaseController.java
+++ src/main/java/com/wsj/wechat/controller/WechatBaseController.java
@@ -187,6 +187,11 @@ public class WechatBaseController {
                     logger.info("未注册味食家账号!");
 //                    ResultBean<WxCustomer> resultBean = ResultBean.failure("未注册味食家账号!",wxCustomer);
                     response.sendRedirect(WsjTools.getDomainName(request)+SysConstants.WebLoginPath.getName());
+                    PrintWriter out = response.getWriter();
+                    out.print("{\"isWechat\":\"true\"}");
+                    out.flush();
+                    out.close();
+//                    out.print(new Gson().toJson(resultBean));
 //                    String contentType = "application/json";
 //                    response.setContentType(contentType);
 //                    PrintWriter out = response.getWriter();
